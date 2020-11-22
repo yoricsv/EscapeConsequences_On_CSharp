5d2248950775fa3f5617e5a2432926e6ece48897
Сергей <yoric@live.ru>
2020-11-22 19:27:22 +0300

    Добавьте файлы проекта.

---
+++ Escape_consequences/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+  *****************************************************************************************
+  *                                     It's C# Code                                      *
+  *****************************************************************************************
+*/
+// USING CODE BEGIN //
+using System;
+// USING CODE END //
+
+// OUR NAMESPACE AREA BEGIN //
+namespace EscapeConsequences
+{
+	// CLASS DEFINITIONS BEGIN //
+	class MainClass		
+	{
+		// MAIN FUNCTION BEGIN //
+		public static void Main(string[] args)
+		{
+			// CODE/INSTRUCTIONS BEGIN //
+/*
+  *****************************************************************************************
+  *                                ESCAPE CONSEQUENCES                                    *
+  *****************************************************************************************
+  *                                                                                       *
+  * \r - return cursor to line start                                                      *
+  * \n (end line) - go to the pointer on next line (analogue [<< endl] )                  *
+  * \t (tabulation) - make an indent between symbols                                      *
+  * \b - delete last displayed symbol                                                     *
+  * \a - sound signal                                                                     *
+  *                                                                                       *
+  * \\ - displays a back slash                                                            *
+  * \" - displays double quotes                                                           *
+  * \' - displays single quote                                                            *
+  *                                                                                       *
+  *****************************************************************************************
+*/
+			// FORMATTING OUTPUT IN CONSOLE BY ESCAPE CONSEQUENCES BEGIN //
+			Console.WriteLine("\t\t Текст \"В лесу родилась елочка\"   ");
+			Console.WriteLine("\t\t (Автор слов Р.Кудашева)          \n");
+			Console.WriteLine("\t\t В лесу родилась елочка,            ");
+			Console.WriteLine("\t\t В лесу она росла,                  ");
+			Console.WriteLine("\t\t Зимой и летом стройная,            ");
+			Console.WriteLine("\t\t Зеленая была.                    \n");
+			Console.WriteLine("\t\t Метель ей пела песенку :           ");
+			Console.WriteLine("\t\t \"Спи, елочка, бай-бай!\"          ");
+			Console.WriteLine("\t\t Мороз снежком укутывал :           ");
+			Console.WriteLine("\t\t \"Смотри, не замерзай!\"         \n");
+			Console.WriteLine("\t\t Трусишка - зайка серенький         ");
+			Console.WriteLine("\t\t Под елочкой скакал,                ");
+			Console.WriteLine("\t\t Порою волк, сердитый волк,         ");
+			Console.WriteLine("\t\t Рысцою пробегал.                 \n");
+			Console.WriteLine("\t\t Чу!Снег по лесу частому            ");
+			Console.WriteLine("\t\t Под полозом скрипит;               ");
+			Console.WriteLine("\t\t Лошадка мохноногая                 ");
+			Console.WriteLine("\t\t Торопится, бежит.                \n");
+			Console.WriteLine("\t\t Везет лошадка дровенки,            ");
+			Console.WriteLine("\t\t А в дровнях - мужичок,             ");
+			Console.WriteLine("\t\t Срубил он нашу елочку              ");
+			Console.WriteLine("\t\t Под самый корешок.               \n");
+			Console.WriteLine("\t\t Теперь она, нарядная,              ");
+			Console.WriteLine("\t\t На праздник к нам пришла           ");
+			Console.WriteLine("\t\t И много, много радостей            ");
+			Console.WriteLine("\t\t Детишкам принесла.                 ");
+			// FORMATTING OUTPUT IN CONSOLE BY ESCAPE CONSEQUENCES END //
+
+            // CODE/INSTRUCTIONS END //
+        }
+	    // MAIN FUNCTION END //
+    }
+    // CLASS DEFINITIONS END //
+}
+// OUR NAMESPACE AREA END //
